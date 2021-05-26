c3950817449d86c82244430b6a2a5ae989417026
Abanoub Rafaat <Abanoub_20180002@fci.helwan.edu.eg>
2021-05-27 01:12:17 +0200

    up1

--- CompilerProject/Form1.cs
+++ CompilerProject/Form1.cs
@@ -16,5 +16,15 @@ namespace CompilerProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //compile button
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //browse button
+        }
     }
 }
